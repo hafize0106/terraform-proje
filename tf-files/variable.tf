bc89f5c1f138e9d33e5625081ad0bf2072a3aa36
hfz <hfz0106@gmail.com>
2024-01-04 23:26:04 +0300

    project

---
+++ tf-files/variable.tf
@@ -0,0 +1,17 @@
+variable "git-name" {
+    default = "hafize0106"
+  
+}
+ variable "git-token" {
+    default = "XXXXXXXXXXX"
+   
+ }
+
+variable "key-name" {
+    default = "lastnightkeypair"
+  
+}
+variable "hosted-zone" {
+    default = "hafizetumer.com"
+  
+}
